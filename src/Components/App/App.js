036edaa7ddad800c1eed7fd267d0501b48c89764
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-09 13:36:29 -0600

    work on static component App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
+render() {
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <PlayList />
+        </div>
+      </div>
     </div>
   );
 }
