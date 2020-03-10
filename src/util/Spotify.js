7b3cb0afcf5288714d6d982ba8003478f0431754
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-10 14:42:22 -0600

    import Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,7 +4,7 @@ let accessToken;
 
 const Spotify = {
   getAccessToken() {
-    if(accessToken) {
+    if (accessToken) {
       return accessToken;
     }
 
@@ -12,7 +12,7 @@ const Spotify = {
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if(accessTokenMatch && expiresInMatch) {
+    if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
       const expiresIn = number(expiresInMatch[1]);
       // This clears the parameters allowing us to grab a new access token when it expires
@@ -23,6 +23,29 @@ const Spotify = {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}_ID&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
       window.location = accessUrl;
     }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+        return jsonResponse.tracks.items.map(track => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artist[0].name,
+          album: track.album.name,
+          uri: track.uri
+        }));
+      });
   }
 }
 
