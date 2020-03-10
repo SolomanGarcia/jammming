e80c56e6a845f331a9b28070fb3ef61437430f6c
Soloman Garcia <soloman.garcia@gmail.com>
2020-03-10 12:05:50 -0600

    add onAdd method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,7 +15,19 @@ class App extends React.Component {
                       {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
                       playlistName: 'My playlist',
                       playlistTracks: [{name: 'playlistname1', artist: 'playlistartist1', album: 'playlistalbum1', id: 4},                {name: 'playlistname2', artist: 'playlistartist2', album: 'playlistalbum2', id: 5},                {name: 'playlistname3', artist: 'playlistartist3', album: 'playlistalbum3', id: 6}]
+    };
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
     }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -26,7 +38,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} 
+                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks} />
           </div>
